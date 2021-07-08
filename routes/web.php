2d17153e536b8b17272571ecc600af9641e26f20
= <=>
2021-07-08 16:06:19 +0100

    nomear as rotas no laravel

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/redi',function(){
+    return redirect()->route('url.name');
+});
+
+Route::get('/name-url', function(){
+    return 'Dr. code';
+})->name('url.name');
 /*
 Route::get('/', function () {
     return view('welcome');
